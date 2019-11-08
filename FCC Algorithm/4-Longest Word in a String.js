010782b3aa77c8caa52377d84ffc91a26cd97b6d
Jedo <jtdelapenaii@gmail.com>
2019-11-08 18:04:16 +0800

    largest number

--- FCC Algorithm/4-Longest Word in a String.js
+++ FCC Algorithm/4-Longest Word in a String.js
@@ -1,12 +1,15 @@
-function findLongestWordLength(str) {
-  var words = str.split(' ');
-  var maxLength = 0;
-
-  for (var i = 0; i < words.length; i++) {
-    if (words[i].length > maxLength) {
-      maxLength = words[i].length;
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
     }
+
+    results[n] = largestNumber;
   }
 
-  return maxLength;
+  return results;
 }
