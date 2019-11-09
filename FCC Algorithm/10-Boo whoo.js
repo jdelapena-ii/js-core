2503e2f3fdcee4cc3e54d776041ec5bdd3195457
Jedo <jtdelapenaii@gmail.com>
2019-11-09 23:59:51 +0800

    title case

---
+++ FCC Algorithm/10-Boo whoo.js
@@ -0,0 +1,7 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    return bool;
+  }
+  
+  booWho(null);
+  
---
+++ FCC Algorithm/11-Title Case Sentence.js
@@ -0,0 +1,6 @@
+function titleCase(str) {
+    return str;
+  }
+  
+  titleCase("I'm a little tea pot");
+  
