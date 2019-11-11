f8390a545a626dbd20decd9ec31ec13643dcc37c
Jedo <jtdelapenaii@gmail.com>
2019-11-12 01:15:55 +0800

    largest number

--- FCC Algorithm/5-Return Largest Number in Arrays.js
+++ FCC Algorithm/5-Return Largest Number in Arrays.js
@@ -1,7 +1,15 @@
 function largestOfFour(arr) {
-    // You can do this!
-    return arr;
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
   }
-  
-  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
-  
+
+  return results;
+}
