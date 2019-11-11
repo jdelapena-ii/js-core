0e6da369bb21f96f531b385c9fb234018313bad5
Jedo <jtdelapenaii@gmail.com>
2019-11-12 00:39:40 +0800

    longest word

--- FCC Algorithm/4-Longest Word in a String.js
+++ FCC Algorithm/4-Longest Word in a String.js
@@ -1,15 +1,5 @@
-function largestOfFour(arr) {
-  var results = [];
-  for (var n = 0; n < arr.length; n++) {
-    var largestNumber = arr[n][0];
-    for (var sb = 1; sb < arr[n].length; sb++) {
-      if (arr[n][sb] > largestNumber) {
-        largestNumber = arr[n][sb];
-      }
-    }
-
-    results[n] = largestNumber;
-  }
-
-  return results;
+function findLongestWordLength(str) {
+  return str.length;
 }
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
