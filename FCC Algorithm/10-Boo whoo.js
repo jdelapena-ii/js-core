3a632b137631e83c354d6c22dcac9493b91b37a5
Jedo <jtdelapenaii@gmail.com>
2019-11-15 10:44:17 +0800

    boo who

--- FCC Algorithm/10-Boo whoo.js
+++ FCC Algorithm/10-Boo whoo.js
@@ -1,7 +1,5 @@
 function booWho(bool) {
-    // What is the new fad diet for ghost developers? The Boolean.
-    return bool;
-  }
-  
-  booWho(null);
-  
+  return typeof(bool) === "boolean"
+}
+
+booWho(null);
