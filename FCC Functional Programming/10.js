410773cfc420f6e6434a0ccabda004ef440fa265
Jedo <jtdelapenaii@gmail.com>
2019-12-03 23:58:44 +0800

    10

---
+++ FCC Functional Programming/10.js
@@ -0,0 +1,15 @@
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
