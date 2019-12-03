097ce4094eff0696a7152c062fd1433172a1b63d
Jedo <jtdelapenaii@gmail.com>
2019-12-04 00:03:10 +0800

    11

---
+++ FCC Functional Programming/11.js
@@ -0,0 +1,9 @@
+function sliceArray(anim, beginSlice, endSlice) {
+    // Add your code below this line
+  
+  
+    // Add your code above this line
+  }
+  var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  sliceArray(inputAnim, 1, 3);
+  
