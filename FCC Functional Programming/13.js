2c25fafe699d32e40d109f9451d40db84eefc989
Jedo <jtdelapenaii@gmail.com>
2020-01-19 22:48:03 +0800

    13

--- FCC Functional Programming/13.js
+++ FCC Functional Programming/13.js
@@ -1,10 +1,9 @@
-function nonMutatingConcat(original, attach) {
-    // Add your code below this line
-  
-  
-    // Add your code above this line
-  }
-  var first = [1, 2, 3];
-  var second = [4, 5];
-  nonMutatingConcat(first, second);
-  
+function nonMutatingPush(original, newItem) {
+  // Add your code below this line
+  return original.push(newItem);
+
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
