d0cbfaa3d950ba688169de10c2641205645c9b01
Jedo <jtdelapenaii@gmail.com>
2020-01-19 23:04:00 +0800

    16

---
+++ FCC Functional Programming/16.js
@@ -0,0 +1,10 @@
+const squareList = (arr) => {
+    // only change code below this line
+    return arr;
+    // only change code above this line
+  };
+  
+  // test your code
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  console.log(squaredIntegers);
+  
