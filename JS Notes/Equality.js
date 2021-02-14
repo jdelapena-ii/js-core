25c369f2354a3ed3db0cc4df632ea6913c1b0979
Jedo <jtdelapenaii@gmail.com>
2021-02-14 13:47:53 +0800

    additional notes on equality

--- JS Notes/Equality.js
+++ JS Notes/Equality.js
@@ -20,3 +20,22 @@ console.log(obj === str); // false
 console.log(null === undefined); // false
 console.log(obj === null); // false
 console.log(obj === undefined); // false
+
+// Loose equality using ==
+
+var num = 0;
+var obj = new String('0');
+var str = '0';
+
+console.log(num == num); // true
+console.log(obj == obj); // true
+console.log(str == str); // true
+
+console.log(num == obj); // true
+console.log(num == str); // true
+console.log(obj == str); // true
+console.log(null == undefined); // true
+
+// both false, except in rare cases
+console.log(obj == null);
+console.log(obj == undefined);
