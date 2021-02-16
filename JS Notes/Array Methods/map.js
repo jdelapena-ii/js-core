a9c53e51d8d17e6291618e88a40924800b5c7cf7
Jedo <jtdelapenaii@gmail.com>
2021-02-16 20:25:05 +0800

    map method

---
+++ JS Notes/Array Methods/map.js
@@ -0,0 +1,55 @@
+// The map() method creates a new array with the results of calling a function for every array element.
+// The map() method calls the provided function once for each element in an array, in order.
+// Note: map() does not execute the function for array elements without values.
+// Note: this method does not change the original array.
+
+const numbers = [1, 3, 5, 6, 9];
+
+const numbersDouble = numbers.map(double);
+// map<number>(callbackfn: (value: number, index: number, array: number[]) => number, thisArg?: any)
+function double(value, index, array){
+   return value * 2;
+}
+
+const numTriple = numbers.map(value => value * 3)
+
+const timesIndex = numbers.map((value, index) => value * index)
+
+console.log(numbersDouble);
+console.log(numTriple);
+console.log(timesIndex);
+
+// another example. mapping over an array of objects.
+
+const products = [
+    {
+        item: "laptop",
+        price: 2000,
+        count: 5
+    },
+    {
+        item: "kindle",
+        price: 60,
+        count: 20
+    },
+    {
+        item: "iphone",
+        price: 1000,
+        count: 20
+    }
+];
+
+const productTotalValue = products.map(item => item.price * item.count)
+console.log(productTotalValue);
+
+const newObj = products.map(item => ({gadget: item.item, total: item.price * item.count}));
+console.log(newObj);
+
+
+// another example. converting an array of strings of numbers into integers
+const strings = ['20', '193', '500', '99']
+const newNumbers = strings.map(item => Number(item));
+const newNums = strings.map(Number);
+console.log(newNumbers);
+console.log(newNums);
+
