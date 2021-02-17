2ad93e9c2b2015f672c3bce7be0e6561ce0fc8c1
Jedo <jtdelapenaii@gmail.com>
2021-02-17 23:56:39 +0800

    fetch api code

---
+++ JS Notes/Async JS/9-fetch-api.js
@@ -0,0 +1,10 @@
+// fetch api
+// the promise is only ever rejected when we get a network error
+fetch('endpoint.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    console.log(data);
+}).catch((err) => {
+    console.log('rejected', err);
+});
