a67fe85eb7072b4d0945f7e0ba17eb88a314268e
Jedo <jtdelapenaii@gmail.com>
2021-02-21 02:45:28 +0800

    fetch api concept

--- JS Notes/Async JS/9-fetch-api.js
+++ JS Notes/Async JS/9-fetch-api.js
@@ -1,7 +1,10 @@
 // fetch api
+// this is already a promise. the fetch api method is returning a promise
 // the promise is only ever rejected when we get a network error
-fetch('endpoint.json').then((response) => {
+
+fetch('todos/jedo.json').then((response) => {
     console.log('resolved', response);
+    // response.json returns a promise which when resolved gives us the data that we need.
     return response.json();
 }).then(data => {
     console.log(data);
