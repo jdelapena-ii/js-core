6107b8c04a10a67954ecf89f297bd19e6411b55a
Jedo <jtdelapenaii@gmail.com>
2021-02-23 00:07:35 +0800

    throwing errors in async await syntax

--- JS Notes/Async JS/10-async-await.js
+++ JS Notes/Async JS/10-async-await.js
@@ -4,12 +4,16 @@
 
 const getTodos = async () => { // this is now an asynchronous function that always return a promise
     const response = await fetch('todos/jedo.json') // the fetch returns a promise. the await keyword stores js and stops it from assigning a value to the response variable until the promise has resolved
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data'); // returnning a new error object
+    }
     const data = await response.json();
     return data;
 }
 
 getTodos()
-    .then(data => console.log('resolved:', data));
+    .then(data => console.log('resolved:', data))
+    .catch(error => console.log('rejected:', error.message))
 
 
 // fetch('todos/jedo.json').then((response) => {
